87f42c6bbdb0f86e5c95a19ee3aa3e99d2014349
Mikkel <m.loekke@m2p.dk>
2021-02-19 12:55:09 +0100

    Fixed.

--- webservice/src/main/java/dk/acto/fafnir/server/providers/SamlProvider.java
+++ webservice/src/main/java/dk/acto/fafnir/server/providers/SamlProvider.java
@@ -4,15 +4,16 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.saml2.provider.service.registration.InMemoryRelyingPartyRegistrationRepository;
 
 @EnableWebSecurity
 @ConditionalOnProperty(name = {"SAML_IDP"})
 public class SamlProvider extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        var repo = RelyingPartyRegistration
+        var repo = new InMemoryRelyingPartyRegistrationRepository();
 
         http.authorizeRequests().anyRequest().authenticated()
-                .and().saml2Login().relyingPartyRegistrationRepository()
+                .and().saml2Login().relyingPartyRegistrationRepository(repo);
     }
 }
